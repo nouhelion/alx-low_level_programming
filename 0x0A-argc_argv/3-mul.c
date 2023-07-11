b4a99bd875740d08e45ac4646aef035fb996195f
Nouhelion <etu_kamel.nouhaila@emsi-edu.ma>
2023-07-11 23:41:27 +0100

    meh

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,54 +1,64 @@
 #include <stdio.h>
 #include "main.h"
-/**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- * Return: the int converted from the string
- */
+#include <limits.h>
 
-int main(int argc, char *argv[])
-{
-	int a, b;
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	a = _atoi(argv[1]);
-	b = _atoi(argv[2]);
-	printf("%d\n", a * b);
-	return (0);
-}
-#include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
 
 /**
- * main - Program that takes first two integer arguments and prints the product
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 1 if not enough arguments passed in, 0 otherwise
+ * print_number - print an integer
+ * @n: the integer to print
+ *
+ * Return: void
  */
-
-int main(int argc, char *argv[])
+void print_number(int n)
 {
-	int i, j;
-
-	if (argc == 1 || argc == 2)
+	if (n > -1)
 	{
-		printf("Error\n");
-		return (1);
+		if (n > 9)
+			print_number(n / 10);
+		_putchar('0' + n % 10);
 	}
 	else
 	{
-		j = 1;
-
-		for (i = 1; i < 3; i++)
-			j *= atoi(argv[i]);
-
-		printf("%d\n", j);
+		_putchar('-');
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
 	}
+}
+
+
+/**
+ * _puts - print a string, followed by a new line
+ * @str: the string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
+
+
+/**
+ * main - print the product of two numbers
+ * @argc: size of the argument vector
+ * @argv: program name and arguments
+ *
+ * Return: 1 if called with anything other than 2 arguments,
+ * otherwise 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		_puts("Error");
+		return (1);
+	}
+
+	print_number(atoi(argv[1]) * atoi(argv[2]));
+	_putchar('\n');
 
 	return (0);
 }
